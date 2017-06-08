3d360b4d15fe46a7fe755f717ccdec0c19dcacf2
Andrew Lee <andrewsoohwanlee@gmail.com>
2017-06-08 16:32:06 -0700

    Push for demo.
    - Fix to initial loading of object of interest not using proper function call.
    - Added two new scenes: Mountain Shrine and Mini Egg.

--- Assets/_Scripts/OculusRemote.cs
+++ Assets/_Scripts/OculusRemote.cs
@@ -10,42 +10,4 @@ public class OculusRemote : MonoBehaviour {
             UnityEngine.VR.InputTracking.Recenter();
         }
     }
-
-    /*// White dot cursor
-    public GameObject cursor;
-
-    // Main Camera
-    public GameObject mainCamera;
-
-	// Use this for initialization
-	void Start () {
-        cursor.SetActive(true);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        RaycastHit hitInfo;
-
-        OverlaySwitch();
-
-        //OVROverlay currentOverlay = GetComponent<OVROverlay>();
-
-        Physics.Raycast(mainCamera.transform.position, mainCamera.transform.forward, out hitInfo, 500);
-
-        //if (hitInfo.collider != null)
-        //{
-            //cursor.transform.position = hitInfo.point;
-        //} else {
-            cursor.transform.position = mainCamera.transform.forward * 26 + mainCamera.transform.position;
-        //}
-
-        cursor.transform.LookAt(mainCamera.transform.position); cursor.transform.LookAt(mainCamera.transform.position);
-    }
-
-    private void OverlaySwitch ()
-    {
-        // Hide the cursor if the overlay is called
-        bool isMenuActive = OVRInspector.instance.IsMenuActive();
-        //cursor.transform.localScale = isMenuActive ? new Vector3(0, 0, 0) : new Vector3(0.3f, 0.3f, 0.3f);
-    }*/
 }
